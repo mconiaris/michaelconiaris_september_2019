f7d5255e8daf42eac8c57fd819f4234d130c2e01
mconiaris <mconiaris@gmail.com>
2019-09-17 19:41:36 -0400

    Changed root to welcome#home

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'welcome/about'
   get 'welcome/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
 end
